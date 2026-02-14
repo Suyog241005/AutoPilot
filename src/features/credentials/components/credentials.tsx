eb94577fa530f5672f05c45dba6ae4288e8ab581
Suyog241005 <suyoghabbu@gmail.com>
2026-02-14 13:05:36 +0530

    test

---
+++ src/features/credentials/components/credentials.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { formatDistanceToNow } from "date-fns";
+import {
+  EmptyView,
+  EntityContainer,
+  EntityHeader,
+  EntityItem,
+  EntityList,
+  EntitySearch,
+  EntitytPagination,
+  ErrorView,
+  LoadingView,
+} from "@/components/entity-components";
+import {
+  useRemoveCredential,
+  useSuspenseCredentials,
+} from "../hooks/use-credentials";
+import { useRouter } from "next/navigation";
+import { useCredentialsParams } from "../hooks/use-credentials-params";
+import { useEntitySearch } from "@/hooks/use-entity-search";
+import { Credential, CredentialType } from "@/generated/prisma/client";
+import Image from "next/image";
+
+export const CredentialsSearch = () => {
+  const [params, setParams] = useCredentialsParams();
+  const { searchValue, onSearchChange } = useEntitySearch({
+    params,
+    setParams,
+  });
+
+  return (
+    <EntitySearch
+      value={searchValue}
+      onChange={onSearchChange}
+      placeholder="Search Credentials"
+    />
+  );
+};
+
+export const CredentialsList = () => {
+  const credentials = useSuspenseCredentials();
+
+  return (
+    <EntityList
+      items={credentials.data.items}
+      getKey={(credential) => credential.id}
+      renderItem={(credential) => <CredentialItem data={credential} />}
+      emptyView={<CredentialsEmpty />}
+    />
+  );
+};
+
+export const CredentialsHeader = ({ disabled }: { disabled?: boolean }) => {
+  return (
+    <EntityHeader
+      title="Credentials"
+      description="Create and manage your credentials"
+      newButtonHref={"/credentials/new"}
+      newButtonLabel="Create Credential"
+      disabled={disabled}
+    />
+  );
+};
+
+export const CredentialsPagination = () => {
+  const credentials = useSuspenseCredentials();
+  const [params, setParams] = useCredentialsParams();
+
+  return (
+    <EntitytPagination
+      disabled={credentials.isFetching}
+      page={credentials.data.page}
+      totalPages={credentials.data.totalPages}
+      onPageChange={(page) => setParams({ ...params, page })}
+    />
+  );
+};
+
+export const CredentialsContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <EntityContainer
+      header={<CredentialsHeader />}
+      search={<CredentialsSearch />}
+      pagination={<CredentialsPagination />}
+    >
+      {children}
+    </EntityContainer>
+  );
+};
+
+export const CredentialsLoading = () => {
+  return <LoadingView message="Loading Credentials..." />;
+};
+
+export const CredentialsError = () => {
+  return <ErrorView message="Error loading Credentials" />;
+};
+
+export const CredentialsEmpty = () => {
+  const router = useRouter();
+
+  const handleCreate = () => {
+    router.push("/credentials/new");
+  };
+  return (
+    <EmptyView
+      message="You haven't created any credentials yet. Get started by creating your first credential."
+      emptyTitle="No Credentials"
+      addLabel="Create Credential"
+      onNew={handleCreate}
+    />
+  );
+};
+
+const credentialLogos: Record<CredentialType, string> = {
+  [CredentialType.OPENAI]: "/logos/openai.svg",
+  [CredentialType.ANTHROPIC]: "/logos/anthropic.svg",
+  [CredentialType.GEMINI]: "/logos/gemini.svg",
+};
+
+export const CredentialItem = ({ data }: { data: Credential }) => {
+  const removeCredential = useRemoveCredential();
+
+  const handleRemove = () => {
+    removeCredential.mutate({ id: data.id });
+  };
+
+  const logo = credentialLogos[data.type] || "/logos/openai.svg";
+
+  return (
+    <EntityItem
+      href={`/credentials/${data.id}`}
+      title={data.name}
+      subTitle={
+        <>
+          Updated {formatDistanceToNow(data.updatedAt, { addSuffix: true })}{" "}
+          &bull; Created {""}{" "}
+          {formatDistanceToNow(data.createdAt, { addSuffix: true })}
+        </>
+      }
+      image={
+        <div className="size-8 flex items-center justify-center ">
+          <Image src={logo} alt={data.type} width={20} height={20} />
+        </div>
+      }
+      onRemove={handleRemove}
+      isRemoving={removeCredential.isPending}
+    ></EntityItem>
+  );
+};
---
+++ src/features/credentials/hooks/use-credentials.ts
@@ -0,0 +1,108 @@
+/**
+ * Hook to fetch all credentials using suspense
+ */
+import { useTRPC } from "@/trpc/client";
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { toast } from "sonner";
+import { useCredentialsParams } from "./use-credentials-params";
+import { CredentialType } from "@/generated/prisma/enums";
+
+export const useSuspenseCredentials = () => {
+  const trpc = useTRPC();
+  const [params] = useCredentialsParams();
+
+  return useSuspenseQuery(trpc.credentials.getMany.queryOptions(params));
+};
+
+/**
+ * Hook to create new credential
+ */
+export const useCreateCredential = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.create.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential "${data.name}" created successfully`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({}),
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failed to create credential: ${error.message}`);
+      },
+    }),
+  );
+};
+
+/**
+ * Hook to remove a credential
+ */
+export const useRemoveCredential = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.remove.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential ${data.name} removed`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({}),
+        );
+        queryClient.invalidateQueries(
+          trpc.credentials.getOne.queryFilter({ id: data.id }),
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failed to remove credential: ${error.message}`);
+      },
+    }),
+  );
+};
+
+/**
+ * Hook to fetch one credential using suspense
+ */
+export const useSuspenseCredential = (id: string) => {
+  const trpc = useTRPC();
+  return useSuspenseQuery(trpc.credentials.getOne.queryOptions({ id }));
+};
+
+/**
+ * Hook to update a credential
+ */
+export const useUpdateCredential = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.update.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential "${data.name}" saved`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({}),
+        );
+        queryClient.invalidateQueries(
+          trpc.credentials.getOne.queryFilter({ id: data.id }),
+        );
+      },
+      onError: (error) => {
+        toast.error(`Failed to update credential: ${error.message}`);
+      },
+    }),
+  );
+};
+
+/**
+ * Hook to fetch credentials by type
+ */
+export const useGetCredentialByType = (type: CredentialType) => {
+  const trpc = useTRPC();
+  return useQuery(trpc.credentials.getByType.queryOptions({ type }));
+};
